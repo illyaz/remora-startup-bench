e59b053ff0a328a438d861cf14cb042f13577b12
Illyaz <illyaz.ptts@gmail.com>
2026-03-08 13:27:33 +0700

    initial

---
+++ RemoraStartupBench/CompilePatch.cs
@@ -0,0 +1,44 @@
+﻿namespace RemoraStartupBench;
+
+using FastExpressionCompiler;
+using HarmonyLib;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Linq.Expressions;
+
+[HarmonyPatch]
+public static class CreatePropertyWriterPatch
+{
+    public static MethodBase TargetMethod()
+    {
+        var type = typeof(Remora.Rest.Json.DataObjectConverter<,>)
+            .Assembly
+            .GetType("Remora.Rest.Json.Reflection.ExpressionFactoryUtilities")!;
+
+        return type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static)
+            .Single(m => m.Name == "CreatePropertyWriter");
+    }
+
+    public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        foreach (var instruction in instructions)
+        {
+            if (instruction.opcode == OpCodes.Callvirt
+                && instruction.operand is MethodInfo { Name: nameof(LambdaExpression.Compile) })
+                yield return new CodeInstruction(OpCodes.Call,
+                    AccessTools.Method(typeof(CompileHelper), nameof(CompileHelper.Compile)));
+            else
+                yield return instruction;
+        }
+    }
+}
+
+public static class CompileHelper
+{
+    public static bool UseFEC = false;
+
+    public static Delegate Compile(LambdaExpression expression) =>
+        UseFEC
+            ? expression.CompileFast() ?? expression.Compile()
+            : expression.Compile();
+}
---
+++ RemoraStartupBench/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using HarmonyLib;
+using Remora.Discord.API.Abstractions.Gateway.Events;
+using Remora.Discord.API.Gateway.Events;
+using Remora.Rest.Extensions;
+using RemoraStartupBench;
+
+BenchmarkRunner.Run<Bench>();
+
+[SimpleJob(RuntimeMoniker.Net90)]
+[SimpleJob(RuntimeMoniker.Net10_0)]
+public class Bench
+{
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        new Harmony("benchmark.fec")
+            .PatchAll();
+    }
+
+    [Benchmark]
+    public JsonSerializerOptions Original()
+    {
+        CompileHelper.UseFEC = false;
+        var options = new JsonSerializerOptions();
+        options.AddDataObjectConverter<IGuildCreate, GuildCreate>();
+        return options;
+    }
+
+    [Benchmark(Baseline = true)]
+    public JsonSerializerOptions FastCompiler()
+    {
+        CompileHelper.UseFEC = true;
+        var options = new JsonSerializerOptions();
+        options.AddDataObjectConverter<IGuildCreate, GuildCreate>();
+        return options;
+    }
+}
